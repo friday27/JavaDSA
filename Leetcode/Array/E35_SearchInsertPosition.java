add8c85dfde4f2fd8a4516f0189b9a6ba6a91619
friday27 <yiyinghahalin@gmail.com>
2019-08-16 18:31:06 +0800

    +Leetcode/Array/35,66 and update filenames

---
+++ Leetcode/Array/E35_SearchInsertPosition.java
@@ -0,0 +1,12 @@
+//Time: O(n)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums[0] >= target)
+            return 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] >= target)
+                return i;
+        }
+        return nums.length;
+    }
+}
