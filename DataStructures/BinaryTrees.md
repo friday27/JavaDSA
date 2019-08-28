f9f6ad23db90642db2a12cfca87b73fca809bf11
friday27 <yiyinghahalin@gmail.com>
2019-08-28 16:46:15 +0800

    add BT note

---
+++ DataStructures/BinaryTrees.md
@@ -0,0 +1,70 @@
+# Binary Trees (BT) and Binary Search Trees (BST)
+
+### Terminology
+* A tree is an undirected graph which satisfies any of the following definitions:
+    * An acyclic (non-cycle) connected graph
+    * A connected graph with N nodes and N-1 edges
+    * An graph in which any 2 vertices are connected by exactly 1 path
+
+* A BST is a binary tree that satisfies the BST invariant: left subtree has smaller elements and right subtree has larger elements.
+
+### Complexity Analysis
+
+        +-------------+-----------+-------+
+        | Operation   | Avg       | Worst |
+        +-------------+-----------+-------+
+        | Insert      | O(log(n)) | O(n)  |
+        +-------------+-----------+-------+
+        | Delete      | O(log(n)) | O(n)  |
+        +-------------+-----------+-------+
+        | Remove      | O(log(n)) | O(n)  |
+        +-------------+-----------+-------+
+        | Search      | O(log(n)) | O(n)  |
+        +-------------+-----------+-------+
+        * The worst case: All the adding elements are greater than the previous one, or all the adding elements are less than the previous one
+
+### Adding elements to a BST
+The inserting operation is a process of comparison:
+* (< case) recursive down left subtree
+* (> case) recursive down right subtree
+* (= case) handle find a duplicate value
+* (found a null leaf) create a new node
+
+### Removing elements from a BST
+It can be seen as a 2 step process:
+1. **Find** the element to be removed (if it exists)
+2. **Replace** the node to be removed with it's successor (if any) to maintain the BST in variant. Below are 4 cases of removing phase:
+    1. The node is a leaf node -> just remove it!
+    2. The node has a right subtree -> the successor would be the root node of right subtree
+    3. The node has a left subtree -> the successor would be the root node of left subtree
+    4. The node has both left and right subtrees -> the successor could be either the last node of left subtree or the first node of right subtree, since it would be the seperating number of left and right subtrees
+
+### Tree Traversals
+The 3 types of traversals are naturally defined recursively:
+* preorder: order from left to right, top to bottom
+
+        preorder(node):
+            if node == null: return
+            print(node.node)
+            preorder(node.left)
+            preorder(node.right)
+
+* **inorder**: order ascending by values
+
+        inorder(node):
+            if node == null: return
+            inorder(node.left)
+            print(node.node)
+            inorder(node.right)
+
+* postorder
+
+        postorder(node):
+            if node == null: return
+            postorder(node.left)
+            postorder(node.right)
+            print(node.node)
+
+* level order traversal
+    * To print the nodes as they appear 1 layer at a time
+    * Use a queue and poll from the queue and offer the child nodes of the removed node to the queue, until the queue is empty
---
+++ DataStructures/implementation/BinarySearchTree.java
@@ -0,0 +1,121 @@
+public class BinarySearchTree <T extends Comparable<T>>{
+    private int nodeCount = 0;
+    private Node root = null;
+
+    public Node() {
+        T data;
+        Node left, right;
+        Node(Node left, Node right, T elem) {
+            this.data = elem;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return nodeCount;
+    }
+
+    public boolean add(T elem) {
+        if(contains(elem))
+            return false; //not accept duplicate values
+        else {
+            root = add(root, elem);
+            nodeCount++;
+            return true;
+        }
+    }
+
+    private Node add(Node node, T elem) {
+        if(node == null)
+            node = new Node(null, null, elem);
+        else {
+            if(elem.compareTo(node.data) < 0)
+                node.left = add(node.left, elem);
+            else
+                node.right = add(node.right, elem);
+        }
+        return node;
+    }
+
+    public boolean remove(T elem) {
+        if(contains(elem)) {
+            root = remove(root, elem);
+            nodeCount--;
+            return true;
+        }
+        return false;
+    }
+
+    private Node remove(Node node, T elem) {
+        if(node == null)
+            return null;
+        int cmp = elem.compareTo(node.data);
+        if(cmp < 0)
+            node.left = remove(node.left, elem);
+        else if(cmp > 0)
+            node.right = remove(node.right, elem);
+        else {
+            if(node.left == null) {
+                Node rightChild = node.right;
+                node.data = null;
+                node = null;
+                return rightChild;
+            }else if(node.right == null) {
+                Node.leftChild = node.left;
+                node.data = null;
+                node = null;
+                return leftChild;
+            }else {
+                Node tmp = digLeft(node.right);
+                node.data = tmp.data;
+                node.right = remove(node.right, tmp.data);
+            }
+        }
+        return node;
+    }
+
+    private Node digLeft(Node node) {
+        Node cur = node;
+        while(cur.left != null)
+            cur = cur.left;
+        return cur;
+    }
+
+    private Node digRight(Node node) {
+        Node cur = node;
+        while(cur.right != null)
+            cur = cur.right;
+        return cur;
+    }
+
+    public boolean contains(T elem) {
+        return contains(root, elem);
+    }
+
+    private boolean contains(Node node, T elem) {
+        if(node == null)
+            return false;
+        int cmp = elem.compareTo(node.data);
+        if(cmp < 0)
+            return contains(node.left, elem);
+        else if(cmp > 0)
+            return contains(node.right, elem);
+        else
+            return true;
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node node) {
+        if(node == null)
+            return 0;
+        return Math.max(height(node.left), height(node.right))+1;
+    }
+}
